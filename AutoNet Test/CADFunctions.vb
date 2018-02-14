22bf0fab89d4a66496a2f882a0e9690f5a5df7a6
Andi Andreas Hjortshøj Rosenhave <ahr@proeng.dk>
2018-02-14 13:50:19 +0100

    Add project files.

---
+++ AutoNet Test/CADFunctions.vb
@@ -0,0 +1,152 @@
+﻿Class CADFunctions
+    Function PickPoint(Msg As String) As Autodesk.AutoCAD.Geometry.Point3d
+        Dim myDoc As Autodesk.AutoCAD.ApplicationServices.Document
+        myDoc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument
+        Dim myEditor As Autodesk.AutoCAD.EditorInput.Editor = myDoc.Editor
+        Dim myPPR As Autodesk.AutoCAD.EditorInput.PromptPointResult
+        myPPR = myEditor.GetPoint(Msg)
+        If myPPR.Status = Autodesk.AutoCAD.EditorInput.PromptStatus.OK Then
+            Return myPPR.Value
+        Else
+            Return Nothing
+        End If
+    End Function
+
+    Function PickPoint(Msg As String, BasePoint As Autodesk.AutoCAD.Geometry.Point3d) As Autodesk.AutoCAD.Geometry.Point3d
+        Dim myDoc As Autodesk.AutoCAD.ApplicationServices.Document
+        myDoc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument
+        Dim myEditor As Autodesk.AutoCAD.EditorInput.Editor = myDoc.Editor
+        Dim myPPO As New Autodesk.AutoCAD.EditorInput.PromptPointOptions(Msg)
+        myPPO.BasePoint = BasePoint
+        myPPO.UseBasePoint = True
+        Dim myPPR As Autodesk.AutoCAD.EditorInput.PromptPointResult
+        myPPR = myEditor.GetPoint(myPPO)
+        If myPPR.Status = Autodesk.AutoCAD.EditorInput.PromptStatus.OK Then
+            Return myPPR.Value
+        Else
+            Return Nothing
+        End If
+    End Function
+
+    Function DrawLine(dbIn As Autodesk.AutoCAD.DatabaseServices.Database,
+        X1 As Double, Y1 As Double, Z1 As Double, X2 As Double, Y2 As Double, Z2 As Double) _
+        As Autodesk.AutoCAD.DatabaseServices.ObjectId
+
+        Using myTrans As Autodesk.AutoCAD.DatabaseServices.Transaction = dbIn.TransactionManager.StartTransaction
+            Dim myBTR As Autodesk.AutoCAD.DatabaseServices.BlockTableRecord =
+                dbIn.CurrentSpaceId.GetObject(Autodesk.AutoCAD.DatabaseServices.OpenMode.ForWrite)
+            Dim stPt As New Autodesk.AutoCAD.Geometry.Point3d(X1, Y1, Z1)
+            Dim enPt As New Autodesk.AutoCAD.Geometry.Point3d(X2, Y2, Z2)
+            Dim newLine As New Autodesk.AutoCAD.DatabaseServices.Line(stPt, enPt)
+            myBTR.AppendEntity(newLine)
+            myTrans.AddNewlyCreatedDBObject(newLine, True)
+            myTrans.Commit()
+            Return newLine.ObjectId
+        End Using
+    End Function
+
+    Function PickEntity(Msg As String) As Autodesk.AutoCAD.DatabaseServices.ObjectId
+        Dim myDoc As Autodesk.AutoCAD.ApplicationServices.Document
+        myDoc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument
+        Dim myEditor As Autodesk.AutoCAD.EditorInput.Editor = myDoc.Editor
+        Dim myPEO As New Autodesk.AutoCAD.EditorInput.PromptEntityOptions(Msg)
+        Dim myPER As Autodesk.AutoCAD.EditorInput.PromptEntityResult
+        myPER = myEditor.GetEntity(myPEO)
+        If myPER.Status = Autodesk.AutoCAD.EditorInput.PromptStatus.OK Then
+            Return myPER.ObjectId
+        Else
+            Return Nothing
+        End If
+    End Function
+
+    Function SelectBlocksOnScreen() As Autodesk.AutoCAD.DatabaseServices.ObjectId()
+        Dim myDoc As Autodesk.AutoCAD.ApplicationServices.Document
+        myDoc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument
+        Dim myEditor As Autodesk.AutoCAD.EditorInput.Editor = myDoc.Editor
+        Dim myTVs(0) As Autodesk.AutoCAD.DatabaseServices.TypedValue
+        myTVs(0) = New Autodesk.AutoCAD.DatabaseServices.TypedValue(0, "INSERT")
+        Dim myFilter As New Autodesk.AutoCAD.EditorInput.SelectionFilter(myTVs)
+        Dim myPSR As Autodesk.AutoCAD.EditorInput.PromptSelectionResult
+        myPSR = myEditor.GetSelection(myFilter)
+        If myPSR.Status = Autodesk.AutoCAD.EditorInput.PromptStatus.OK Then
+            Return myPSR.Value.GetObjectIds
+        Else
+            Return Nothing
+        End If
+    End Function
+
+    Function SelectAllBlocks() As Autodesk.AutoCAD.DatabaseServices.ObjectId()
+        Dim myDoc As Autodesk.AutoCAD.ApplicationServices.Document
+        myDoc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument
+        Dim myEditor As Autodesk.AutoCAD.EditorInput.Editor = myDoc.Editor
+        Dim myTVs(0) As Autodesk.AutoCAD.DatabaseServices.TypedValue
+        myTVs(0) = New Autodesk.AutoCAD.DatabaseServices.TypedValue(0, "INSERT")
+        Dim myFilter As New Autodesk.AutoCAD.EditorInput.SelectionFilter(myTVs)
+        Dim myPSR As Autodesk.AutoCAD.EditorInput.PromptSelectionResult
+        myPSR = myEditor.SelectAll(myFilter)
+        If myPSR.Status = Autodesk.AutoCAD.EditorInput.PromptStatus.OK Then
+            Return myPSR.Value.GetObjectIds
+        Else
+            Return Nothing
+        End If
+    End Function
+
+    Function GetBlockAttributes(BlockID As Autodesk.AutoCAD.DatabaseServices.ObjectId) As Dictionary(Of String, String)
+        Dim myD As New Dictionary(Of String, String)
+        Using myTrans As Autodesk.AutoCAD.DatabaseServices.Transaction =
+                BlockID.Database.TransactionManager.StartTransaction()
+            Dim myBlockRef As Autodesk.AutoCAD.DatabaseServices.BlockReference =
+                BlockID.GetObject(Autodesk.AutoCAD.DatabaseServices.OpenMode.ForRead)
+            If myBlockRef.AttributeCollection.Count > 0 Then
+                For Each myAttRefID As Autodesk.AutoCAD.DatabaseServices.ObjectId In myBlockRef.AttributeCollection
+                    Dim myAttRef As Autodesk.AutoCAD.DatabaseServices.AttributeReference = myAttRefID.GetObject(Autodesk.AutoCAD.DatabaseServices.OpenMode.ForRead)
+                    If myD.ContainsKey(myAttRef.Tag) = False Then
+                        myD.Add(myAttRef.Tag, myAttRef.TextString)
+                    End If
+                Next
+            End If
+            myTrans.Dispose()
+        End Using
+        Return myD
+    End Function
+
+    Function GetBlockProperties(ObjID As Autodesk.AutoCAD.DatabaseServices.ObjectId) As Dictionary(Of String, Object)
+        Dim myD As New Dictionary(Of String, Object)
+        Using myTrans As Autodesk.AutoCAD.DatabaseServices.Transaction =
+                ObjID.Database.TransactionManager.StartTransaction
+            Dim myEnt As Autodesk.AutoCAD.DatabaseServices.BlockReference =
+                ObjID.GetObject(Autodesk.AutoCAD.DatabaseServices.OpenMode.ForRead)
+            For Each myPInfo As System.Reflection.PropertyInfo In GetType(Autodesk.AutoCAD.DatabaseServices.BlockReference).GetProperties
+                myD.Add(myPInfo.Name, myPInfo.GetValue(myEnt))
+            Next
+        End Using
+        Return myD
+    End Function
+
+    Function GetBTRProperties(ObjID As Autodesk.AutoCAD.DatabaseServices.ObjectId) As Dictionary(Of String, Object)
+        Dim myD As New Dictionary(Of String, Object)
+        Using myTrans As Autodesk.AutoCAD.DatabaseServices.Transaction =
+                ObjID.Database.TransactionManager.StartTransaction
+            Dim myEnt As Autodesk.AutoCAD.DatabaseServices.BlockTableRecord =
+                ObjID.GetObject(Autodesk.AutoCAD.DatabaseServices.OpenMode.ForRead)
+            For Each myPInfo As System.Reflection.PropertyInfo In GetType(Autodesk.AutoCAD.DatabaseServices.BlockTableRecord).GetProperties
+                myD.Add(myPInfo.Name, myPInfo.GetValue(myEnt))
+            Next
+        End Using
+        Return myD
+    End Function
+
+    Sub WriteToTextFile(TextToWrite As String, FilePath As String)
+        Dim mySW As New IO.StreamWriter(FilePath)
+        mySW.WriteLine(TextToWrite)
+        mySW.Close()
+        mySW.Dispose()
+    End Sub
+
+    Sub AppendToTextFile(TextToWrite As String, FilePath As String)
+        Dim mySW As New IO.StreamWriter(FilePath, True)
+        mySW.WriteLine(TextToWrite)
+        mySW.Close()
+        mySW.Dispose()
+    End Sub
+End Class
---
+++ AutoNet Test/Class1.vb
@@ -0,0 +1,115 @@
+﻿Imports Autodesk.AutoCAD
+Imports Autodesk.AutoCAD.Interop
+Imports Autodesk.AutoCAD.Interop.Common
+Imports Autodesk.AutoCAD.DatabaseServices
+Imports Autodesk.AutoCAD.Runtime
+Imports Autodesk.AutoCAD.Geometry
+Imports Autodesk.AutoCAD.ApplicationServices
+Imports Autodesk.AutoCAD.EditorInput
+Imports Autodesk.AutoCAD.Colors
+
+Public Class Class1
+    Dim CAD As New CADFunctions
+    Dim ed As Editor = Core.Application.DocumentManager.MdiActiveDocument.Editor
+    Dim curSelection As SelectionSet
+    Dim sr As PromptSelectionResult = ed.SelectImplied()
+
+    'Enable referencing the active window as "ThisDrawing" throughout the code.
+    Public ReadOnly Property ThisDrawing As AcadDocument
+        Get
+            Return DocumentExtension.GetAcadDocument(Core.Application.DocumentManager.MdiActiveDocument)
+        End Get
+    End Property
+
+    'Enable the use of command MBORD to call the subroutine "CreateMTextBorder"
+    <CommandMethod("MBORD")>
+    Public Sub CreateMTextBorder()
+        Dim ent As AcadEntity 'The Text entity
+        Dim pnt As Object 'The coordinate list or "PickedPoints" 
+        Try
+            ThisDrawing.Utility.GetEntity(ent, pnt, "Vælg en MText") 'Try to retrieve the text entity and coordinates by prompting user selection.
+            Dim mt As IAcadMText = ent
+            Dim boundMin, boundMax As Object
+            Dim coords(7) As Double
+            mt.GetBoundingBox(boundMin, boundMax)
+            '0-1 = Bottom Left Corner      2-3 = Bottom Right Corner       4-5 = Top Right Corner      6-7 = Top Left Corner
+            coords(0) = boundMin(0) - 1
+            coords(1) = boundMin(1) - 1
+            coords(2) = boundMax(0) + 1
+            coords(3) = boundMin(1) - 1
+            coords(4) = boundMax(0) + 1
+            coords(5) = boundMax(1) + 1
+            coords(6) = boundMin(0) - 1
+            coords(7) = boundMax(1) + 1
+
+            'Draw the border in it's appropiate space.
+            If ThisDrawing.ActiveSpace = AcActiveSpace.acModelSpace Then
+                ThisDrawing.ModelSpace.AddLightWeightPolyline(coords).Closed = True
+            Else
+                ThisDrawing.PaperSpace.AddLightWeightPolyline(coords).Closed = True
+            End If
+        Catch ex As Exception
+            ThisDrawing.Utility.Prompt("Ugyldigt valg.")
+        End Try
+    End Sub
+
+    <CommandMethod("GridAlign", CommandFlags.Redraw + CommandFlags.UsePickSet)>
+    Public Sub GridAlign()
+        If sr.Status = PromptStatus.OK Then
+            curSelection = sr.Value
+        End If
+    End Sub
+
+    <CommandMethod("StartPID", CommandFlags.NoPaperSpace + CommandFlags.NoPerspective)>
+    Public Sub PIDSetup()
+        Application.DocumentManager.MdiActiveDocument.Database.Orthomode = True
+        Application.SetSystemVariable("SNAPMODE", 1)
+        Application.SetSystemVariable("GRIDUNIT", 0.5)
+    End Sub
+
+    <CommandMethod("PFT", CommandFlags.UsePickSet + CommandFlags.Redraw + CommandFlags.Modal)>
+    Public Sub ToggleGrouping()
+
+        Dim curObjs As ObjectIdCollection
+        Dim arObjIds() As ObjectId
+
+        If (sr.Status = PromptStatus.OK) Then
+            curSelection = sr.Value
+            curObjs = New ObjectIdCollection(sr.Value.GetObjectIds())
+            ed.WriteMessage("Antal objekter valgt: " & sr.Value.Count.ToString())
+        Else
+            curObjs = New ObjectIdCollection()
+            ed.WriteMessage("Vælg nogle objekter")
+            sr = ed.GetSelection()
+            If (sr.Status = PromptStatus.OK) Then
+                ed.WriteMessage("Antal nye objekter valgt: " & sr.Value.Count.ToString())
+                For Each objId As ObjectId In sr.Value.GetObjectIds()
+                    curObjs.Add(objId)
+                Next
+                curObjs.CopyTo(arObjIds, 0)
+                'ed.SetImpliedSelection(tSelectionSet)
+                For Each obj As ObjectId In curObjs
+                    Debug.WriteLine(obj.ToString())
+                Next
+
+            End If
+        End If
+    End Sub
+
+    <CommandMethod("tGroup", CommandFlags.UsePickSet + CommandFlags.Modal + CommandFlags.Redraw)>
+    Public Sub tGroup()
+        Dim sObjs As ObjectIdCollection
+        Dim grp As Group = New Group("", True)
+
+        If sr.Status = PromptStatus.OK Then
+            curSelection = sr.Value
+            sObjs = New ObjectIdCollection(sr.Value.GetObjectIds())
+
+
+        Else
+            ed.WriteMessage("You must have some objects, or a group selected before toggle grouping.")
+        End If
+
+    End Sub
+
+End Class
